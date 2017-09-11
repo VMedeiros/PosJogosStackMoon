5d92cd5777e15a01fd6acb366fe708017b789ae5
Victor Medeiros <victor.leitao95@gmail.com>
2017-09-11 16:50:04 -0300

    Stack Game Made With Unity 5.6

---
+++ StackMoon/Assets/Scripts/Rotacao.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+public class Rotacao : MonoBehaviour
+{
+    public float volta = 50;
+    void Update()
+    {
+        transform.Rotate(Vector3.up * volta * Time.deltaTime);
+    }
+
+}
